01f55a9f19ab77bca41fc932d4570f52e55aca2e
cmert2017 <cihanmert2075@gmail.com>
2020-10-31 12:43:53 -0400

    printmsg commit

--- src/main/java/HalloweenTest.java
+++ src/main/java/HalloweenTest.java
@@ -1,7 +1,17 @@
 public class HalloweenTest {
     public static void main(String[] args) {
 
+
         System.out.println("hello Test");
+
+        System.out.println("hello Test1");
+        System.out.println("hello Test2");
+        System.out.println("hello Test3");
+
+        //some more changes goes here
+
+
+
 
     }
 }
